e9ea12d4a1aeda70a4ed57da7433321dc0da75c4
Arafath <rsarafath.signin@gmail.com>
2026-02-21 07:38:03 +0600

    project:I complete money section,for ex:"add money,send money,pay bill etc"

---
+++ script/login.js
@@ -0,0 +1,23 @@
+document.getElementById('btn-login').addEventListener('click', function () {
+  // 1)get the mobile number
+  const loginNumber = document.getElementById('login-number');
+  const number = loginNumber.value;
+  
+  //2)get the pin
+  const loginPin = document.getElementById('login-pin');
+  const PIN = loginPin.value;
+
+  //3 match number and pin
+  if (number == '01893176269' && PIN == '1234') {
+    alert('Login Successfull')
+    //destination after login
+    window.location.assign('/home.html')
+  } else {
+    alert('login faild');
+    loginNumber.value = '';
+    loginPin.value = '';
+    return;
+
+  }
+  
+});
